0f1e2b1fd175dbac89dacd2784502869ac550ce0
Nathanael Abel <75358980+nathanaelabel@users.noreply.github.com>
2023-11-11 09:19:20 +0700

    feat: add route page

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/welcome', function () {
     return view('welcome');
+});
+
+Route::get('/home', function () {
+    return view('home');
+});
+
+Route::get('/', function () {
+    return view('index');
 });
