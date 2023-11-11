f1da07b580e0356030c29da68ac0b861b657cad7
Nathanael Abel <75358980+nathanaelabel@users.noreply.github.com>
2023-11-11 09:18:40 +0700

    feat: landing page layout

---
+++ resources/views/home.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Whisker List | Home</title>
+</head>
+
+<body>
+    <div class="container">
+        <header>
+            <h1>Whisker List</h1>
+            <p>A playful way to organize your tasks with feline charm 🐾</p>
+        </header>
+
+        <main>
+            <section class="cta">
+                <h2>Embrace Productivity with a Purr-sonal Touch</h2>
+                <p>Download Whisker List now and make task management delightful!</p>
+                <a href="#" class="download-button">Download Now</a>
+            </section>
+
+            <section class="features">
+                <h2>Key Features</h2>
+                <ul>
+                    <li>🐾 Cat-Themed Task Management</li>
+                    <li>🗓️ Kitty-Kalendar Integration</li>
+                    <li>🌟 Purr-sonalized Experience</li>
+                    <li⚡ AgilePaws Technology</li>
+                </ul>
+            </section>
+        </main>
+
+        <footer>
+            <p>&copy; 2023 Whisker List. All rights reserved.</p>
+        </footer>
+    </div>
+</body>
+
+</html>
